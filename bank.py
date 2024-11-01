33bd3baa66a8d3a95e76c828244108cb50ba01a6
MEHDI MAURON <161387406+MeMAURON@users.noreply.github.com>
2024-11-01 10:02:55 +0330

    p7

--- bank.py
+++ bank.py
@@ -1,7 +1,7 @@
 customers = []
 # Hafeze etelaat
 while True:
-
    print("")
    print("Please select the desired option:  :")      
    print("1. Eftetahe hesab: ")
+   print("2. Afzayeshe mojudi ")
